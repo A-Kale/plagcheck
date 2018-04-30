713b88f3532132fb56a70aeb554320a29b4b089b
Abolaji Kalejaiye <a.kalejaiye@lancaster.ac.uk>
2018-04-30 16:59:10 +0100

    Created a program that reads lines of texts from files and counts the number of 'the's there are.

---
+++ plagchecker.c
@@ -0,0 +1,28 @@
+#include <stdio.h
+
+int main()
+{
+	FILE myFileHandle;
+	
+	myFileHandle = fopen("Project3TestFiles/test1.txt", "r");
+	
+	int theCounter; // Counts the number of times the appears within the file.
+	str the[4]; = 'the';
+	
+	
+	
+	if (myFileHandle != NULL)
+	{
+		char textLine[90]; // This is where I will temporarily store the lines that I get from the text.
+		
+		while (fgets(textLine, 90, myFileHandle) != NULL)
+		{
+			if(strstr(textLine, the) != NULL)
+			{
+				theCounter++;
+			}
+		}
+		fclose(myFileHandle);
+	}
+	printf("the number of 'the's inside of this file is %d.\n", theCounter);
+}
