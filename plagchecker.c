0de93a48cd7e2533991a7e9c6f634f02876f2d9d
Kalejaiye <kalejaiy@egb076000037.lancs.local>
2018-05-16 17:04:42 +0100

    Added the structure to the program, instead of a character array.

--- plagchecker.c
+++ plagchecker.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
+struct Word {
+	char name[4];
+	int counter;
+};
+
 int main()
 {
 	FILE *myFileHandle;
@@ -8,36 +13,41 @@ int main()
 	myFileHandle = fopen("Project3TestFiles/test1.txt", "r");
 	
 	int theCounter; // Counts the number of times the appears within the file.
-	char the[4] = "the";
 	
-	
+	struct Word The; // Delcares The of the type Word.
+	strcpy(The.name, "the");
+	The.counter = 0;
 	
 	if (myFileHandle != NULL)
 	{
-		char textLine[200]; // This is where I will temporarily store the lines that I get from the text.
+		char textLine[270]; // This is where I will temporarily store the lines that I get from the text.
 		
-		while (fgets(textLine, 200, myFileHandle) != NULL)
+		while (fgets(textLine, 270, myFileHandle) != NULL)
 		{
 			printf("Line read is: %s\n", textLine);
 			for (int i=0; i<strlen(textLine); i++)
 			{
-				if (textLine[i] == 116 || textLine[i] == 84)
+				if (textLine[i] != EOF)
 				{
-					if (textLine[i+1] == 104)
+					if (textLine[i] == 116 || textLine[i] == 84)
 					{
-						if (textLine[i+2] == 101)
+						if (textLine[i+1] == 104)
 						{
-							if(textLine[i+3] == 32)
+							if (textLine[i+2] == 101)
 							{
-								theCounter++;
-								// printf("(%c %c %c) ", textLine[i+4], textLine[i+5], textLine[i+6]);
+								if(textLine[i+3] == 32)
+								{
+									The.counter++;
+									printf("(%c %c %c) ", textLine[i+4], textLine[i+5], textLine[i+6]);
+								}
 							}
 						}
 					}
 				}
 			}
+			memset(textLine, '0', strlen(textLine));
 		}
 		fclose(myFileHandle);
 	}
-	printf("the number of 'the's inside of this file is %d.\n", theCounter);
+	printf("the number of 'the's inside of this file is %d.\n", The.counter);
 }
